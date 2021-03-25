e61a1828562b31b84ae7bb240e2b04604d6dc72e
dejagraver <dejaamaartin@gmail.com>
2021-03-24 22:58:11 -0400

    pseudocoded steps

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Declare Variables 
+    //Create variable to display the date 
+    //create a variable to display the current time 
+    //create a variable to display colors
+    //create a variable array with objects for the time 
+//create a functon that creates the rows 
+    //.forEach to use everything from the array of object
+    //var row =
+    //use jquery
+        //select an element on the page and insert it into another  
+        //$(".container").append(row);
+//Create save button on click
+//Create Local Storage for timeline saved events 
+//Create function that determined color based on time 
+    //if time < present red, else if time is > then present green
+
+
