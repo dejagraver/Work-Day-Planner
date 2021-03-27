7773d02ece9d7e642093a55f05cc10840a787e45
dejagraver <dejaamaartin@gmail.com>
2021-03-26 23:39:38 -0400

    Created rows

--- js/script.js
+++ js/script.js
@@ -1,17 +1,55 @@
+$(function() {
 // Declare Variables 
-    //Create variable to display the date 
-    //create a variable to display the current time 
-    //create a variable to display colors
-    //create a variable array with objects for the time 
-//create a functon that creates the rows 
-    //.forEach to use everything from the array of object
-    //var row =
-    //use jquery
-        //select an element on the page and insert it into another  
-        //$(".container").append(row);
-//Create save button on click
+//value = what they wrote 
+//time =
+ $(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time,value)
+ })
+
+//create function 
+//see what time it is 
+//loop over each time block 
+//check to see if the time block now 
+function currentTime() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function (){
+        var timeBlock = parseInt($(this).attr("id").split("-")[1])
+        if (timeBlock < currentHour) {
+            $(this).addClass("past")
+        } else if (timeBlock === currentHour) {
+            $(this).addClass("present")
+            $(this).removeClass("past")
+        } else {
+            $(this).addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+        }
+    })
+} 
+currentTime();
+
+var interval = setInterval(currentTime, 15000);
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+
+///repeat the var interval 9-5 in 24 hour time 
+//set hour nine to this thing that we put in .val
+//write something and save it 
+//get item out of local storage 
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"))
+
+
+
+  
+
 //Create Local Storage for timeline saved events 
-//Create function that determined color based on time 
+//.this refers to the actual elements
+  //is always a reference to an object and in strict mode can beany value
+ 
+//Create function that determined row color based on time 
     //if time < present red, else if time is > then present green
 
-
+});
